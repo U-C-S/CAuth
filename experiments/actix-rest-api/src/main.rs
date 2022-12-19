5dff74f27cbb741bf95502fa4440e18685838fbe
U-C-S <uchanakyasrinivas@gmail.com>
2022-12-19 16:43:09 +0530

    updates in actix experiment

--- experiments/actix-rest-api/src/main.rs
+++ experiments/actix-rest-api/src/main.rs
@@ -1,13 +1,19 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 async fn index() -> HttpResponse {
     HttpResponse::Ok().body("Hello")
+}
+
+#[get("/echo")]
+async fn echo() -> impl Responder {
+    HttpResponse::Ok().body("Echo")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .service(echo)
             .route("/", web::get().to(index))
             .route("/user", web::post().to(index))
     })
