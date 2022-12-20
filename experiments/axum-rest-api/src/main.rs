25c0a9ef1f6d4aecba5b7a2fcdeb5969d6e6f8ed
U-C-S <uchanakyasrinivas@gmail.com>
2022-12-20 23:20:38 +0530

    init axum experiment

---
+++ experiments/axum-rest-api/src/main.rs
@@ -0,0 +1,17 @@
+async fn say_world() {
+    println!("world");
+}
+
+#[tokio::main]
+async fn main() {
+    // Calling `say_world()` does not execute the body of `say_world()`.
+    let op = say_world();
+
+    // This println! comes first
+    println!("hello");
+
+    // Calling `.await` on `op` starts executing `say_world`.
+    op.await;
+}
+
+async fn nesting_routes() {}
