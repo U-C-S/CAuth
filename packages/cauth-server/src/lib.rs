3d35936a271c75a404e394b80a0f888edcaea796
U-C-S <uchanakyasrinivas@gmail.com>
2022-12-25 19:20:16 +0530

    Initial cauth-server code

--- packages/cauth-server/src/lib.rs
+++ packages/cauth-server/src/lib.rs
@@ -1,5 +1,41 @@
+use std::net::SocketAddr;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub struct AdminOpts {
+    pub access_key: String,
+}
+
+pub struct Server {
+    pub addr: SocketAddr,
+    pub middleware: Option<Vec<Box<dyn Middleware>>>,
+    pub admin: AdminOpts,
+}
+
+impl Server {
+    pub fn new(addr: SocketAddr) -> Self {
+        Self {
+            addr,
+            middleware: None,
+            admin: todo!(),
+        }
+    }
+
+    pub fn add_middleware(&mut self, middleware: Box<dyn Middleware>) {
+        self.middleware.insert(middleware);
+        self
+    }
+
+    pub async fn run(self) {
+        let app = Router::new().nest("/", todo!());
+
+        axum::Server::bind(&self.addr)
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    }
 }
 
 #[cfg(test)]
