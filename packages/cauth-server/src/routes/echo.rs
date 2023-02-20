b325ea9983bb4521a2e451777efe8fb3fff1152c
U-C-S <uchanakyasrinivas@gmail.com>
2023-02-20 18:11:20 +0530

    seperate echo route handler

--- packages/cauth-server/src/routes/echo.rs
+++ packages/cauth-server/src/routes/echo.rs
@@ -1,9 +1,11 @@
+use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
 
 pub fn routes() -> Router {
-    Router::new().route(
-        "/echo",
-        get(|| async { "echo" }).post(|body: String| async { body }),
-    )
+    Router::new().route("/echo", get(|| async { "echo" }).post(echo_response))
+}
+
+pub async fn echo_response(body: String) -> impl IntoResponse {
+    body
 }
