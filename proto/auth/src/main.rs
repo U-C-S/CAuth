5701f7d15a3be4f2c4d0ac0dacc0a9cb57a24442
U-C-S <uchanakyasrinivas@gmail.com>
2023-03-08 18:20:16 +0530

    init server code (proto)

--- proto/auth/src/main.rs
+++ proto/auth/src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
+pub mod routes;
 
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{Router, Server, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3721));
+    println!("Listening on http://{}", addr);
+
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
