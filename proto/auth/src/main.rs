7446d2bee10c33d3a1004e98aaa87c1f064b4c01
U-C-S <uchanakyasrinivas@gmail.com>
2023-04-02 16:20:35 +0530

    working adding data to global state obj

--- proto/auth/src/main.rs
+++ proto/auth/src/main.rs
@@ -1,14 +1,35 @@
+pub mod models;
 pub mod routes;
 
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
 
 use axum::{routing::get, Router, Server};
+use tokio::sync::RwLock;
+
+use crate::models::{Service, State};
 
 #[tokio::main]
 async fn main() {
+  // let serv =
+  let state = State {
+    services: HashMap::from([(
+      "google".to_string(),
+      Service {
+        api: "google".to_string(),
+        description: "google".to_string(),
+        user: "lol".to_string(),
+      },
+    )]),
+    users: HashMap::new(),
+  };
+  let shared_state = Arc::new(RwLock::new(state));
+
   let app = Router::new()
     .route("/", get(|| async { "Hello, World!" }))
-    .nest("/service", routes::service_management::service_routes())
+    .nest(
+      "/service",
+      routes::service_management::service_routes(shared_state),
+    )
     .nest("/api", routes::service_api::service_api_routes());
 
   let addr = SocketAddr::from(([127, 0, 0, 1], 3721));
@@ -19,3 +40,5 @@ async fn main() {
     .await
     .unwrap();
 }
+
+fn lol() {}
--- proto/auth/src/routes/service_management.rs
+++ proto/auth/src/routes/service_management.rs
@@ -1,14 +1,29 @@
-use axum::{routing::get, Router};
+use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
 
-pub fn service_routes() -> Router {
+use crate::models::{self, Service, SharedState};
+
+pub fn service_routes(state: models::SharedState) -> Router<()> {
   Router::new()
     .route("/admin", get(get_service_info))
     .route("/user", get(get_service_info))
+    .with_state(state)
 }
 
 #[axum_macros::debug_handler]
-async fn get_service_info() -> &'static str {
-  "Get service info"
+async fn get_service_info(State(state): State<SharedState>) -> impl IntoResponse {
+  let x = &mut state.write().await.services;
+  let y = format!("{:?}", x);
+
+  x.insert(
+    "hi".to_string(),
+    Service {
+      api: "hi".to_string(),
+      description: "hi".to_string(),
+      user: "lol".to_string(),
+    }
+  );
+
+  Json(y)
 }
 
 async fn add_service() -> &'static str {
