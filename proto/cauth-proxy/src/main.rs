c6a24687e0396c776f64ff54234fdc04e5e1121d
U-C-S <uchanakyasrinivas@gmail.com>
2023-06-04 20:39:59 +0530

    misc proxy updates (wont work)

--- proto/cauth-proxy/src/main.rs
+++ proto/cauth-proxy/src/main.rs
@@ -2,7 +2,12 @@ mod state;
 
 use std::net::SocketAddr;
 
-use axum::{routing::get, Router, Server};
+use axum::{
+  extract::State,
+  http::{HeaderMap, Method, StatusCode},
+  routing::{any, get},
+  Router, Server,
+};
 use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
@@ -11,6 +16,7 @@ async fn main() {
 
   let app = Router::new()
     .route("/", get(|| async { "Echo!" }))
+    .route("/query", any(handler))
     .layer(CorsLayer::new().allow_origin(Any).allow_headers(Any))
     .with_state(pg_pool);
 
@@ -21,4 +27,25 @@ async fn main() {
     .serve(app.into_make_service())
     .await
     .unwrap();
+}
+
+async fn handler(headers: HeaderMap, method: Method, State(pg_state): State<state::PgSqlPool>) {
+  let req_service_name = headers.get("service-name").unwrap().to_str().unwrap();
+  let req_endpoint = headers.get("service-endpoint").unwrap().to_str().unwrap();
+  let req_headers = headers.get("service-headers").unwrap();
+
+  println!("{}", method);
+
+  let conn = pg_state.get().await.unwrap();
+
+  let preparee = conn
+    .prepare("SELECT api_base_url FROM ServiceTable WHERE service_name=$1")
+    .await
+    .unwrap();
+
+  let sql_params: Vec<String> = vec![req_service_name.into()];
+  let api_url = conn.query_raw(
+    "SELECT api_base_url FROM ServiceTable WHERE service_name=$1",
+    sql_params,
+  );
 }
--- proto/cauth-proxy/src/state.rs
+++ proto/cauth-proxy/src/state.rs
@@ -2,15 +2,18 @@ use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
 use tokio_postgres::NoTls;
 
-pub fn create_redis_instance() {
-  todo!("create_redis_instance")
-}
+// pub fn create_redis_instance() {
+//   todo!("create_redis_instance")
+// }
 
 pub type PgSqlPool = Pool<PostgresConnectionManager<NoTls>>;
 
 pub async fn create_postgres_instance() -> PgSqlPool {
-  let manager =
-    PostgresConnectionManager::new_from_stringlike("host=localhost user=postgres", NoTls).unwrap();
+  let manager = PostgresConnectionManager::new_from_stringlike(
+    "host=localhost user=postgres password=3721`",
+    NoTls,
+  )
+  .unwrap();
   let pool = Pool::builder().build(manager).await.unwrap();
 
   pool
